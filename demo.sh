4767703efd51ef82865cba663418c5c07f5ab267
bleedingmouse <liuxuehao@gmail.com>
2018-02-27 16:47:30 +0800

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ALBUMID="12546834"
-RSSFILE="fafaking.xml"
+ALBUMID="14393"
+RSSFILE="ydl.xml"
 # export PBUUID=""
-./ximalaya_linux_amd64 $ALBUMID > $fafaking.xml
+./lizhi_linux_amd64 $ALBUMID > $ydl.xml
 ls
-curl -X PUT -F c=@$fafaking.xml https://ptpb.pw/$PBUUID
+curl -X PUT -F c=@$ydl.xml https://ptpb.pw/$PBUUID
